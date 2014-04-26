9b69174207cca6910485a91df980894ae2f4980d
aaarel <aarel@gmail.com>
2014-04-26 14:46:27 +0300

    name changes Ariel

--- JAVA_MVP/src/controller/Presenter.java
+++ JAVA_MVP/src/controller/Presenter.java
@@ -1,9 +1,16 @@
-package presentor;
+package controller;
 
 import java.util.Observable;
 import java.util.Observer;
 
+import model.Model;
+import view.View;
+
 public class Presenter implements Observer {
+	//data members of View and Model type that will be initialized in the 'Main program'
+	View ui;
+	Model m;
+	
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
