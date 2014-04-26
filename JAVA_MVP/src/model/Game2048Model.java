78536d2d298ad90c66a9792b186cf2a300fbb7a2
aaarel <aarel@gmail.com>
2014-04-26 14:28:52 +0300

    Ariel -adding classes and interfaces (infrastructor to work with)

---
+++ JAVA_MVP/src/model/Game2048Model.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.util.Observable;
+
+public class Game2048Model extends Observable implements Model {
+
+	@Override
+	public void moveUp() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void moveDown() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void moveLeft() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void moveRight() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int[][] getData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ JAVA_MVP/src/view/Game2048View.java
@@ -0,0 +1,21 @@
+package view;
+
+import java.util.Observable;
+
+import model.Model;
+
+public class Game2048View extends Observable implements View  {	
+
+	@Override
+	public void displayData(int[][] data) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getUserCommand() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
