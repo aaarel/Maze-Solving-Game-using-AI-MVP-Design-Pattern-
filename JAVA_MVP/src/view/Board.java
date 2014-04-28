0cf568e1fa4f63cf3747706cb366f81f77166300
aaarel <aarel@gmail.com>
2014-04-28 19:32:07 +0300

    Addinhg paint listners

--- JAVA_MVP/src/view/Board.java
+++ JAVA_MVP/src/view/Board.java
@@ -1,11 +1,20 @@
 package view;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 
 public class Board extends Composite {
 
+	int N; 				//represents the size of the board N over N
+	int [][] BoardData; //represents the data of the board
 	public Board(Composite parent, int style) {
 		super(parent, style);
+		N=4;
+		BoardData = new int[N][N];
+		setLayout( new GridLayout(N, true) );
+		setLayoutData( new GridLayout(N,true) );
 		
 	}
 
--- JAVA_MVP/src/view/Tile.java
+++ JAVA_MVP/src/view/Tile.java
@@ -1,22 +1,42 @@
 package view;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 
 public class Tile extends Canvas{
 
+	public int value;
+	
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+		ChangeBackgroundColor(); //when value is changed - background color will change also
+		redraw();
+	}
+
 	public Tile(Composite parent, int style) {
 		super(parent, style);
-		// TODO Auto-generated constructor stub
+		value=0;
+		Font f=getFont();
+		Font nf=new Font(getDisplay(),f.getFontData()[0].getName(),16,SWT.BOLD);
+		setFont(nf);
 		addPaintListener(new PaintListener() {
 			
 			@Override
-			public void paintControl(PaintEvent arg0) {
-				// TODO Auto-generated method stub
-				
+			public void paintControl(PaintEvent e) {
+				if(value==0)
+					setBackground(getDisplay().getSystemColor(SWT.COLOR_CYAN));
+				else
+					setBackground(getDisplay().getSystemColor(SWT.COLOR_WHITE));
 			}
+			
 		});
 	}
 	
@@ -24,7 +44,7 @@ public class Tile extends Canvas{
 		
 	}
 	
-	private void ChangeColor()
+	private void ChangeBackgroundColor()
 	{
 		
 	}
